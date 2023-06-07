40262f86cd8405929baa973ec1a09f57951965dc
Robbin van Ooij <r.vanooij@ltp.nl>
2023-06-07 13:36:45 +0200

    finish Games test

--- tests/integration/Games.spec.js
+++ tests/integration/Games.spec.js
@@ -3,6 +3,7 @@ import Games from './../../src/views/Games/Games.vue';
 import { Swiper, SwiperSlide } from 'swiper/vue';
 import { slidesMock } from './slides.mock';
 
+// mock the store and provide mock data and method
 jest.mock('./../../src/stores/games', () => {
     const { gamesMock } = require('./games.mock');
 
@@ -14,6 +15,7 @@ jest.mock('./../../src/stores/games', () => {
     };
 });
 
+// mock the Swiper instance and provide mock content for the slides
 jest.mock('swiper/vue', () => {
     const { slidesMock } = require('./slides.mock');
 
@@ -46,7 +48,6 @@ jest.mock('swiper/vue', () => {
 
 describe('Games', () => {
     it('allows navigation between slides', async () => {
-        const setSomeStatePropMock = jest.fn(); // Mock the custom function
         const nextSlideMock = jest.fn();
 
         const wrapper = mount(Games, { // eslint-disable-line
@@ -59,7 +60,6 @@ describe('Games', () => {
             stubs: {
                 Swiper: {
                     methods: {
-                        setSomeStateProp: setSomeStatePropMock,
                         nextSlide: nextSlideMock
                     },
                 },
@@ -75,22 +75,21 @@ describe('Games', () => {
         const componentInstance = wrapper.vm;
 
         // Assign the mock function to the component method
-        componentInstance.setSomeStateProp = setSomeStatePropMock;
         componentInstance.nextSlide = nextSlideMock;
 
-        let loader;
+        let loaderElement;
 
         // Ensure there is a loader
-        loader = wrapper.find('.loader');
-        expect(loader.exists()).toBe(true);
+        loaderElement = wrapper.find('.loader');
+        expect(loaderElement.exists()).toBe(true);
 
         // Simulate the completion of loading
         wrapper.vm.loading = false;
         await wrapper.vm.$nextTick();
 
-        // Ensure there is no loader
-        loader = wrapper.find('.loader');
-        expect(loader.exists()).toBe(false);
+        // Ensure there is no loader now that loading is completed
+        loaderElement = wrapper.find('.loader');
+        expect(loaderElement.exists()).toBe(false);
 
         // Ensure there is a Swiper instance
         const swiperComponentWrapper = wrapper.findComponent(Swiper);
@@ -100,23 +99,19 @@ describe('Games', () => {
         const nextButton = wrapper.find('.button.right .next');
         expect(nextButton.exists()).toBe(true);
 
-        // Assert the number of slides in the mock
-        const slides = slidesMock;
-        expect(slides.length).toBe(2);
-
         // Assert the number of rendered slides
         const renderedSlides = swiperComponentWrapper.findAllComponents(SwiperSlide);
-        expect(renderedSlides.length).toBe(slides.length);
+        expect(renderedSlides.length).toBe(slidesMock.length);
 
         // Ensure the next slide button is present
         const nextSlideButton = wrapper.find('.testRight');
         expect(nextSlideButton.exists()).toBe(true);
 
-        // navigate to the next slide
+        // Navigate to the next slide
         await nextSlideButton.trigger('click');
         await wrapper.vm.$nextTick();
 
-        // ensure the function to go to the next slide is called once
+        // Ensure the function to go to the next slide is called once
         expect(nextSlideMock).toHaveBeenCalledTimes(1);
     });
 });
